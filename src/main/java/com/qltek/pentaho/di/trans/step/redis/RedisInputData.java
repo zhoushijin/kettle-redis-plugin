303dd33b6ce3059a789be74c1ec873854e42dc85
zhoushijin <zhoushijinlife@gmail.com>
2019-11-05 22:31:34 +0800

    version 1

---
+++ src/main/java/com/qltek/pentaho/di/trans/step/redis/RedisInputData.java
@@ -0,0 +1,22 @@
+package com.qltek.pentaho.di.trans.step.redis;
+
+import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.trans.step.BaseStepData;
+import org.pentaho.di.trans.step.StepDataInterface;
+
+
+/**
+ * @author Matt Burgess
+ */
+public class RedisInputData extends BaseStepData implements StepDataInterface {
+
+    public RowMetaInterface outputRowMeta;
+
+    /**
+     *
+     */
+    public RedisInputData() {
+        super();
+    }
+
+}
